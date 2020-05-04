225bd50e853ae260ba387bf2ad455807b55e31cf
tabascospicy <rafaeltabasca@gmail.com>
2020-05-03 20:54:22 -0400

    final

--- src/ProfileMobile/List/List.js
+++ src/ProfileMobile/List/List.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect, useContext } from "react";
+import React from "react";
+import Info from "./../Info/info"
 import "./index.scss";
 function List({Title,...props}) {
+    
     return(
      <div className="dataBox">
          <div className="titleBox">
                  <h4 className="title">{Title}</h4>
+                 <Info isVisible={false}/>
          </div>
      </div>
     );
